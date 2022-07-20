c8a3531d4b547bbe770c9a5e8ca3f9a8f6fef857
ericbjones <ebj.ericjones@gmail.com>
2022-07-20 10:11:04 -0700

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Vuepress Docs Boilerplate',
+  title: '💩 Vuepress Docs Boilerplate 🚽',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
